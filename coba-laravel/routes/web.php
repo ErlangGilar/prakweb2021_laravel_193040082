1fc51fb34348098547fa8c77768c13d65175f424
ErlangGilar <ErlangGilar@users.noreply.github.com>
2021-10-13 22:42:42 +0700

    Mengikuti dan menyelesaikan video 3, Struktur Folder, Routes & View

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Erlang Gilar",
+        "email"=> "erlang.pamungkas@gmail.com",
+        "image"=> "fotoerlang.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
