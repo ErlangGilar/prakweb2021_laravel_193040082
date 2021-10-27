a47f5e7c74c77275d8eaa8429ffbb5dbf34beb0a
ErlangGilar <ErlangGilar@users.noreply.github.com>
2021-10-27 12:31:23 +0700

    Mengikuti video 9, Database Seeder

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,62 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Erlang Gilar',
+            'email'=> 'erlang.pamungkas@gmail.com',
+            'password'=> bcrypt('12345')
+        ]);
+        User::create([
+            'name' => 'Liam Galagher',
+            'email'=> 'Liam.galagher@gmail.com',
+            'password'=> bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title'=> 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aut odio consequuntur voluptates fugit magni',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aut odio consequuntur voluptates fugit magni laboriosam natus nulla voluptatibus illum atque assumenda quo inventore totam expedita molestiae tempore, veritatis repellendus accusantium delectus. Voluptas quae quidem sequi placeat maiores, fuga quos tenetur molestias dolorem! Obcaecati, officia ex laboriosam odio cupiditate eveniet autem in tempora possimus, fugiat earum minus. Iste voluptatum praesentium tenetur ab minus distinctio eius, ipsam temporibus, officiis dolorum commodi nihil libero iusto quia id? Error ullam nostrum sed quas, quo quam voluptatum ipsum distinctio aspernatur accusamus ratione et facere expedita molestias repellat consequatur illo voluptates itaque! Officia, placeat quidem.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title'=> 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aut odio consequuntur voluptates fugit magni',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aut odio consequuntur voluptates fugit magni laboriosam natus nulla voluptatibus illum atque assumenda quo inventore totam expedita molestiae tempore, veritatis repellendus accusantium delectus. Voluptas quae quidem sequi placeat maiores, fuga quos tenetur molestias dolorem! Obcaecati, officia ex laboriosam odio cupiditate eveniet autem in tempora possimus, fugiat earum minus. Iste voluptatum praesentium tenetur ab minus distinctio eius, ipsam temporibus, officiis dolorum commodi nihil libero iusto quia id? Error ullam nostrum sed quas, quo quam voluptatum ipsum distinctio aspernatur accusamus ratione et facere expedita molestias repellat consequatur illo voluptates itaque! Officia, placeat quidem.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title'=> 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aut odio consequuntur voluptates fugit magni',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aut odio consequuntur voluptates fugit magni laboriosam natus nulla voluptatibus illum atque assumenda quo inventore totam expedita molestiae tempore, veritatis repellendus accusantium delectus. Voluptas quae quidem sequi placeat maiores, fuga quos tenetur molestias dolorem! Obcaecati, officia ex laboriosam odio cupiditate eveniet autem in tempora possimus, fugiat earum minus. Iste voluptatum praesentium tenetur ab minus distinctio eius, ipsam temporibus, officiis dolorum commodi nihil libero iusto quia id? Error ullam nostrum sed quas, quo quam voluptatum ipsum distinctio aspernatur accusamus ratione et facere expedita molestias repellat consequatur illo voluptates itaque! Officia, placeat quidem.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title'=> 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aut odio consequuntur voluptates fugit magni',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aut odio consequuntur voluptates fugit magni laboriosam natus nulla voluptatibus illum atque assumenda quo inventore totam expedita molestiae tempore, veritatis repellendus accusantium delectus. Voluptas quae quidem sequi placeat maiores, fuga quos tenetur molestias dolorem! Obcaecati, officia ex laboriosam odio cupiditate eveniet autem in tempora possimus, fugiat earum minus. Iste voluptatum praesentium tenetur ab minus distinctio eius, ipsam temporibus, officiis dolorum commodi nihil libero iusto quia id? Error ullam nostrum sed quas, quo quam voluptatum ipsum distinctio aspernatur accusamus ratione et facere expedita molestias repellat consequatur illo voluptates itaque! Officia, placeat quidem.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -3,11 +3,11 @@
 @section('container')
 <h1 class="mb-5">{{ $post->title }}</h1>
 
-<p>By. Erlang Gilar in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> </p>
+<p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </p>
 
 {!! $post->body !!}
 
 
-<a href="/blog">Back To Post</a>
+<a href="/blog" class="d-block mt-3">Back To Post</a>
 @endsection
 
